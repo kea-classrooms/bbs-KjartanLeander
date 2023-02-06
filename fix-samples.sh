5b74f2f64267730c42b6b8cadaa38f13c89c04d1
KjartanLeander <113175889+KjartanLeander@users.noreply.github.com>
2023-02-06 13:00:10 +0000

    Fix thing

--- fix-samples.sh
+++ fix-samples.sh
@@ -1,27 +1,8 @@
-mkdir samples
-mkdir samples/animals
-mkdir samples/countries
-mkdir samples/movies
-mkdir samples/animals
-
-ANIMALS=('Apes' 'Elephants' 'Elephant babies' 'Monkies' 'apes')
-COUNTRIES=('Denmark' 'Sweden' 'USA' 'France' 'Italy' '.apes' 'North Korea')
-MOVIES=('Batman' 'Sex And The City' 'JFK' 'Poltergeist' 'Germany')
-
-cd samples/animals 
-for id in "${ANIMALS[@]}"
-do
-echo "$id >> $id.txt"
-done
-
-cd countries
-for id in "${COUNTRIES[@]}"
-do
-echo "$id >> $id.txt"
-done
-
-cd movies
-for id in "${MOVIES[@]}"
-do
-echo "$id >> $id.txt"
-done
+ cd samples/
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/
+ cd -
